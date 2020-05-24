a7d67a6fba5e748cc8cf9dc8051cb8996898e076
Barbieri Marcelo <MarceloBarGit@gmail.com>
2020-05-23 22:26:06 -0300

    Incio del Proyecto - original

---
+++ demoMB/src/main/java/com/unla/demoMB/DemoMbApplication.java
@@ -0,0 +1,13 @@
+package com.unla.demoMB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoMbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoMbApplication.class, args);
+	}
+
+}
---
+++ demoMB/src/test/java/com/unla/demoMB/DemoMbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unla.demoMB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
